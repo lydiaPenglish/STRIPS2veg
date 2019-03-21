5bd51c1f3803114d02b2594b799840b8425f7f72
Lydia English <lenglish@iastate.edu>
2019-03-21 10:33:06 -0500

    Updated size for 3 sites. Replaced devtools with usethis

--- data-raw/site_info_and_species_list.R
+++ data-raw/site_info_and_species_list.R
@@ -7,7 +7,7 @@ mutate(
     siteID = factor(toupper(siteID))) %>%
   select(siteID, season_seeded, year_seeded, area_in_strips, seeding_method,
          species_seeded, management, nurse_crop)
-devtools::use_data(all_site_info, overwrite = TRUE)
+usethis::use_data(all_site_info, overwrite = TRUE)
 
 species_list <- read_csv("species_list.csv")
-devtools::use_data(species_list, overwrite = TRUE)
+usethis::use_data(species_list, overwrite = TRUE)
