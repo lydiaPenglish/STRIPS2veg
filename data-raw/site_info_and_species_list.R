6582c663ebe9d5d34fafbc854a8e888669d6186d
Lydia English <lenglish@iastate.edu>
2019-04-19 16:23:02 -0500

    Changes 4.19

--- data-raw/site_info_and_species_list.R
+++ data-raw/site_info_and_species_list.R
@@ -3,10 +3,11 @@ library("tidyr")
 library("readr")
 
 all_site_info <- read_csv("all_site_info.csv") %>%  
-mutate(
-    siteID = factor(toupper(siteID))) %>%
-  select(siteID, season_seeded, year_seeded, area_in_strips, seeding_method,
-         species_seeded, management, nurse_crop)
+    mutate(siteID = factor(toupper(siteID))) %>%
+    select(siteID, season_seeded, year_seeded, area_in_strips, seeding_method,
+         species_seeded, management, nurse_crop)%>%
+  arrange(siteID)
+
 usethis::use_data(all_site_info, overwrite = TRUE)
 
 species_list <- read_csv("species_list.csv")
